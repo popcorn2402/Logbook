276f94afd32d6d195f700b510f83e80f4ffc8ad4
popcorn2402 <pauline.pomas@gmail.com>
2023-01-09 18:36:04 +0100

    DailyLogRepository creation

---
+++ app/src/main/java/com/ar/logbook/model/repository/DailyLogRepository.java
@@ -0,0 +1,32 @@
+package com.ar.logbook.model.repository;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import com.ar.logbook.model.dao.DailyLogDAO;
+import com.ar.logbook.model.entity.DailyLog;
+
+import java.util.List;
+
+class DailyLogRepository {
+
+    private DailyLogDAO mDailyLogDao;
+    private LiveData<List<DailyLog>> mAllDailyLog;
+
+    DailyLogRepository(Application application) {
+        DailyLogRoomDatabase db = DailyLogRoomDatabase.getDatabase(application);
+        mDailyLogDao = db.dailyLogDAO();
+        mAllDailyLog = mDailyLogDao.getAlphabetizedWords();
+    }
+
+    LiveData<List<DailyLog>> getAllDailyLog() {
+        return mAllDailyLog;
+    }
+
+    void insert(DailyLog dailyLog) {
+        DailyLogRoomDatabase.databaseWriteExecutor.execute(() -> {
+            mDailyLogDao.insert(dailyLog);
+        });
+    }
+}
