5feae8350cb22ddb32e1a86520317ac82c8b1a07
popcorn2402 <pauline.pomas@gmail.com>
2023-01-09 18:22:04 +0100

    DailyLogRoomDatabase creation

---
+++ app/src/main/java/com/ar/logbook/model/repository/DailyLogRoomDatabase.java
@@ -0,0 +1,37 @@
+package com.ar.logbook.model.repository;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.ar.logbook.model.dao.DailyLogDAO;
+import com.ar.logbook.model.entity.DailyLog;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {DailyLog.class}, version = 1, exportSchema = false)
+public abstract class DailyLogRoomDatabase extends RoomDatabase {
+
+    public abstract DailyLogDAO dailyLogDAO();
+
+    private static volatile DailyLogRoomDatabase INSTANCE;
+    private static final int NUMBER_OF_THREADS = 4;
+    static final ExecutorService databaseWriteExecutor =
+            Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    static DailyLogRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (DailyLogRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                    DailyLogRoomDatabase.class, "daily_log_database")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
